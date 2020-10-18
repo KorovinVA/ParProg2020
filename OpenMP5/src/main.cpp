6b82568af26949b9ab25a5c116603c6bae5e160f
Vlad <korovin.va@phystech.edu>
2020-10-19 00:35:07 +0300

    OpenMP5 task

--- OpenMP5/src/main.cpp
+++ OpenMP5/src/main.cpp
@@ -3,9 +3,93 @@
 #include <fstream>
 #include <omp.h>
 
+uint8_t isAlive(uint8_t* field, int x, int y, uint32_t xSize, uint32_t ySize)
+{
+  if(x < 0)
+  {
+    x = xSize - 1;
+  }
+  if(y < 0)
+  {
+    y = ySize - 1;
+  }
+  if (x == (int)xSize)
+  {
+    x = 0;
+  }
+  if( y == (int)ySize)
+  {
+    y = 0;
+  }
+
+  return field[y * xSize + x];
+}
+
+uint8_t proceed(uint8_t cell, uint8_t* field, uint32_t x, uint32_t y, uint32_t xSize, uint32_t ySize)
+{
+  uint8_t sum = 0;
+  sum += isAlive(field, int(x - 1), int(y - 1), xSize, ySize);
+  sum += isAlive(field, int(x - 1), int(y),     xSize, ySize);
+  sum += isAlive(field, int(x - 1), int(y + 1), xSize, ySize);
+  sum += isAlive(field, int(x),     int(y - 1), xSize, ySize);
+  sum += isAlive(field, int(x),     int(y + 1), xSize, ySize);
+  sum += isAlive(field, int(x + 1), int(y - 1), xSize, ySize);
+  sum += isAlive(field, int(x + 1), int(y),     xSize, ySize);
+  sum += isAlive(field, int(x + 1), int(y + 1), xSize, ySize);
+
+  uint8_t newCell = 0;
+  if(cell == 0)
+  {
+    if(sum == 3)
+    {
+      newCell = 1;
+    }
+  }
+  else
+  {
+    if(sum == 2 || sum == 3)
+    {
+      newCell = 1;
+    }
+  }
+  return newCell;
+}
+
 void calc(uint32_t xSize, uint32_t ySize, uint32_t iterations, uint32_t num_threads, uint8_t* inputFrame, uint8_t* outputFrame)
 {
 
+  for(uint32_t y = 0; y < ySize; ++y)
+  {
+    for(uint32_t x = 0; x < xSize; ++x)
+    {
+      outputFrame[y * xSize + x] = inputFrame[y * xSize + x];
+    }
+  }
+
+  for(uint32_t i = 0; i < iterations; ++i)
+  {
+    #pragma omp parallel for num_threads(num_threads)
+    for(uint32_t y = 0; y < ySize; ++y)
+    {
+      for(uint32_t x = 0; x < xSize; ++x)
+      {
+        outputFrame[y * xSize + x] = proceed(inputFrame[y * xSize + x],
+                                             inputFrame,
+                                             x,
+                                             y,
+                                             xSize,
+                                             ySize);
+      }
+    }
+
+    for(uint32_t y = 0; y < ySize; ++y)
+    {
+      for(uint32_t x = 0; x < xSize; ++x)
+      {
+        inputFrame[y * xSize + x] = outputFrame[y * xSize + x];
+      }
+    }
+  }
 }
 
 int main(int argc, char** argv)
